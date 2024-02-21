ebb7887a9f25787ace1b377376691900dc5206a4
Parthiv Kumar Das <130219761+parthiv002@users.noreply.github.com>
2024-02-22 00:42:58 +0530

    Different accessible maps added

--- app/src/main/java/com/example/gmap_clone/MainActivity.kt
+++ app/src/main/java/com/example/gmap_clone/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.gmap_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.SearchView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -11,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var mapFragment: SupportMapFragment
-    lateinit var googleMap: GoogleMap
+    lateinit var mgoogleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +22,27 @@ class MainActivity : AppCompatActivity() {
 
         mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
+            mgoogleMap = it
         })
+
+        val mapOptionButton: ImageButton = findViewById(R.id.mapOptionsMenu)
+        val popupMenu = PopupMenu(this, mapOptionButton)
+        popupMenu.menuInflater.inflate(R.menu.map_options, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {menuItem ->
+                changeMap(menuItem.itemId)
+            true
+        }
+        mapOptionButton.setOnClickListener {
+            popupMenu.show()
+        }
+    }
+
+    private fun changeMap(itemId: Int) {
+        when(itemId){
+            R.id.normal_map -> mgoogleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.hybrid_map -> mgoogleMap?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            R.id.satellite_map -> mgoogleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            R.id.terrain_map -> mgoogleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        }
     }
 }
